0349edfb8cd366c8053f83e796cb08356a26b036
James Kuzhilaparambil <rhinosaur2000@gmail.com>
2021-11-02 19:13:37 -0400

    First commit- mongo atlas cloud linked

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+/* File name: db.js   Author: James Kuzhilaparambil  Student ID: 301119040  Web App Name: COMP229-F2021-MidTerm-301119040*/
+
+module.exports = {
+
+
+
+  "URI": "mongodb+srv://james2:57ZldEWDXDxB0u72@mongodbserver2.vzb0t.mongodb.net/book_store?retryWrites=true&w=majority"
+  
+  
+};
